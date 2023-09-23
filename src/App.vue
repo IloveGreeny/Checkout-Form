e79a74762722010b38645cf39041a2b78fdaa84b
IloveGreeny <greenxd23@gmail.com>
2023-09-23 13:20:12 +0400

    msg

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import CheckoutForm from "../src/components/CheckoutForm/CheckoutForm.vue";
+
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+
+      <CheckoutForm/>
+
+
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
